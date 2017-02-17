9c8d2e10d07b4fc32b493f089fb216f834d66e03
aumengse <balanayaerole@gmail.com>
2017-02-17 16:26:49 +0800

    add frmsubject

---
+++ Practice/Forms/frmsubject.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmsubject
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btndelete = New System.Windows.Forms.Button()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.btnadd = New System.Windows.Forms.Button()
+        Me.btnlocked = New System.Windows.Forms.Button()
+        Me.btnedit = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.Panel1.Controls.Add(Me.btndelete)
+        Me.Panel1.Controls.Add(Me.btnclose)
+        Me.Panel1.Controls.Add(Me.btnadd)
+        Me.Panel1.Controls.Add(Me.btnlocked)
+        Me.Panel1.Controls.Add(Me.btnedit)
+        Me.Panel1.Location = New System.Drawing.Point(5, 203)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(600, 53)
+        Me.Panel1.TabIndex = 22
+        '
+        'btndelete
+        '
+        Me.btndelete.Location = New System.Drawing.Point(244, 8)
+        Me.btndelete.Name = "btndelete"
+        Me.btndelete.Size = New System.Drawing.Size(112, 37)
+        Me.btndelete.TabIndex = 2
+        Me.btndelete.Text = "DELETE"
+        Me.btndelete.UseVisualStyleBackColor = True
+        '
+        'btnclose
+        '
+        Me.btnclose.Location = New System.Drawing.Point(480, 8)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(112, 37)
+        Me.btnclose.TabIndex = 3
+        Me.btnclose.Text = "CLOSE"
+        Me.btnclose.UseVisualStyleBackColor = True
+        '
+        'btnadd
+        '
+        Me.btnadd.Location = New System.Drawing.Point(8, 8)
+        Me.btnadd.Name = "btnadd"
+        Me.btnadd.Size = New System.Drawing.Size(112, 37)
+        Me.btnadd.TabIndex = 0
+        Me.btnadd.Text = "ADD"
+        Me.btnadd.UseVisualStyleBackColor = True
+        '
+        'btnlocked
+        '
+        Me.btnlocked.Location = New System.Drawing.Point(362, 8)
+        Me.btnlocked.Name = "btnlocked"
+        Me.btnlocked.Size = New System.Drawing.Size(112, 37)
+        Me.btnlocked.TabIndex = 4
+        Me.btnlocked.Text = "LOCKED"
+        Me.btnlocked.UseVisualStyleBackColor = True
+        '
+        'btnedit
+        '
+        Me.btnedit.Location = New System.Drawing.Point(126, 8)
+        Me.btnedit.Name = "btnedit"
+        Me.btnedit.Size = New System.Drawing.Size(112, 37)
+        Me.btnedit.TabIndex = 1
+        Me.btnedit.Text = "EDIT"
+        Me.btnedit.UseVisualStyleBackColor = True
+        '
+        'frmsubject
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(615, 262)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "frmsubject"
+        Me.Text = "frmsubject"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btndelete As System.Windows.Forms.Button
+    Friend WithEvents btnclose As System.Windows.Forms.Button
+    Friend WithEvents btnadd As System.Windows.Forms.Button
+    Friend WithEvents btnlocked As System.Windows.Forms.Button
+    Friend WithEvents btnedit As System.Windows.Forms.Button
+End Class
---
+++ Practice/Forms/frmsubject.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmsubject
+
+    Private Sub btnadd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnadd.Click
+
+    End Sub
+End Class
